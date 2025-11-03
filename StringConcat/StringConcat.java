f5ef194f144654d5ef73f357096cf672009d7b8b
Brace1000 <brianmoraro500@gmail.com>
2025-11-03 14:49:33 +0300

    fix:update to handle null strings

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,12 +1,5 @@
-
 public class StringConcat {
     public static String concat(String s1, String s2) {
-        // if (s1 == null) {
-        //     s1 = "";
-        // }
-        // if (s2 == null) {
-        //     s2 = "";
-        // }
-        return s1 + s2;
+        return s1 + s2; // This is the simplest and correct way
     }
 }
