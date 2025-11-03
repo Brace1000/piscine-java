c68235aad7830ced5978b7ee7649e29cb7fadd8b
Brace1000 <brianmoraro500@gmail.com>
2025-11-03 15:44:32 +0300

    Add palindrome application

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,19 +1,21 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-        
         if (s == null) {
             return false;
         }
+        
+        // Convert the string to lowercase for case-insensitive comparison
+        String lowerCaseString = s.toLowerCase();
         int left = 0;
-        int right = s.length() - 1;
+        int right = lowerCaseString.length() - 1;
+        
         while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+            if (lowerCaseString.charAt(left) != lowerCaseString.charAt(right)) {
                 return false;
             }
             left++;
             right--;
         }
         return true;
-
     }
 }
