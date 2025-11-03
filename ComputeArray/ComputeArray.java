7e431c390e70801bccd0688cba9e8817b5c56b96
Brace1000 <brianmoraro500@gmail.com>
2025-11-03 15:48:52 +0300

    feat:Add compute array functionality

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,26 @@
+public class ComputeArray {
+    public static int[] computeArray(int[] array) {
+        if (array == null) {
+            return new int[0];
+        }
+        
+        int[] result = new int[array.length];
+        
+        for (int i = 0; i < array.length; i++) {
+            int current = array[i];
+            
+            if (current % 3 == 0) {
+                // Multiple of 3: multiply by 5
+                result[i] = current * 5;
+            } else if (current % 3 == 1) {
+                // Multiple of 3 + 1: increment by 7
+                result[i] = current + 7;
+            } else {
+                // Multiple of 3 + 2: stay unchanged
+                result[i] = current;
+            }
+        }
+        
+        return result;
+    }
+}
