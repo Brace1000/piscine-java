4badb7850d1cbe3f88973b44161479d42288c51e
Brace1000 <brianmoraro500@gmail.com>
2025-11-05 13:57:58 +0300

    Add constructor with four parameters

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,22 @@
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+    
+    // Default constructor - no parameters
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+    
+    // Constructor with four parameters
+    public CelestialObject(String name, double x, double y, double z) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
