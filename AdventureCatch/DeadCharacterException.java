ab934247c2e0dee9341b9926b8daf185e8e6658a
Brace1000 <brianmoraro500@gmail.com>
2025-11-13 11:35:07 +0300

    Add Adventure catch exercise solution

---
+++ AdventureCatch/DeadCharacterException.java
@@ -0,0 +1,24 @@
+public class DeadCharacterException extends Exception {
+    private final Character character;
+    
+    public DeadCharacterException(Character character) {
+        this.character = character;
+    }
+    
+    @Override
+    public String getMessage() {
+        String characterType;
+        
+        if (character instanceof Sorcerer) {
+            characterType = "sorcerer";
+        } else if (character instanceof Templar) {
+            characterType = "templar";
+        } else if (character instanceof Monster) {
+            characterType = "monster";
+        } else {
+            characterType = "character";
+        }
+        
+        return "The " + characterType + " " + character.getName() + " is dead.";
+    }
+}
